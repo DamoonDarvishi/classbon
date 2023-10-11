f2dd50d0337e8c8958070f70337051400d062cf9
Damoon Darvishi <damoon.darvishi@gmail.com>
2023-10-11 16:01:50 +0330

    install colord pckg & config cstom colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Colors } from "./_components/colors/colors";
 
 export default function Home() {
   return (
     <div className='text-purple-500'>
-      Classbon
+      <Colors />
     </div>
   )
 }
