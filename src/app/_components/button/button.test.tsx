4e954c08525e9ca73e0d5408f42cccb9f8a46b7f
Damoon Darvishi <damoon.darvishi@gmail.com>
2023-11-01 16:35:40 +0330

    add hero section component and test

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,23 +1,32 @@
 import { describe } from "node:test";
-import { Button } from "./button"
+import { Button } from "./button";
 import { render, screen } from "@testing-library/react";
 
-describe('Button', () => {
-    test('render a default button', () => {
-        const { getByText } = render(<Button>Click Here</Button>);
-        expect(getByText('Click Here')).toBeInTheDocument()
-    });
+describe("Button", () => {
+  test("render a default button", () => {
+    const { getByText } = render(<Button>Click Here</Button>);
+    expect(getByText("Click Here")).toBeInTheDocument();
+  });
 
-    test('disables the button when isDisable prop is true', () => {
-        render(<Button isDisabled={true}>Click here</Button>);
-        expect(screen.getByRole('button')).toBeDisabled();
-    });
+  test("disables the button when isDisable prop is true", () => {
+    render(<Button isDisabled={true}>Click here</Button>);
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
 
-    test('applies the correct css class for different button variant', () => {
-        const { rerender} = render(<Button variant="primary">Click Here</Button>);
-        expect(screen.getByRole("button")).toHaveClass("btn-primary");
+  test("applies the correct css class for different button variant", () => {
+    const { rerender } = render(<Button variant="primary">Click Here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-primary");
 
-        rerender(<Button variant="info">Click Here</Button>);
-        expect(screen.getByRole("button")).toHaveClass("btn-info");
-    })
-})
+    rerender(<Button variant="info">Click Here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-info");
+  });
+
+  test("show rendered button", () => {
+    render(
+      <Button variant="primary" isOutline={true} size="large" isDisabled={true}>
+        Click Here
+      </Button>
+    );
+    screen.debug();
+  });
+});
---
+++ src/app/_components/home-hero-section/home-hero-section.test.tsx
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import { HomeHeroSection } from "./home-hero-section";
+
+describe("Home hero section", () => {
+  test("display the home hero section", () => {
+    const { container } = render(<HomeHeroSection />);
+    expect(container).toMatchSnapshot(`
+        <section className='bg-hero-pattern bg-no-repeat bg-center mt-5 xl:mt-20 xl:bg-left'>
+            <div className="container flex flex-col-reverse items-center xl:flex-row">
+                <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
+                    <h3 className="text-xl dark:text-info xl-text-2xl">خوش اومدی به ...</h3>
+                    <h1 className="text-3xl font-black gradient lg:text-3xl xl:text-5xl">مسیر صعود به دوره های برنامه نویسی</h1>
+                    <p className="text-lg font-bold leading-8">
+                    هر جای مسیر برنامه نویسی که باشی،
+                    با همراهی استادهای باتجربه کلاسبن میتونی بدون محدودیت به
+                    قله های بالاتر صعود کنی. ما همیشه هواتو داریم.
+                    </p>
+                    <div className="mt-5 flex gap-4">
+                    <Button variant="accent" size="large">دوره های ری اکت و نکست</Button>
+                    <Button variant="neutral" size="large">دوره های ری اکت و نکست</Button>
+                    </div>
+                    <Image src="/images/frameworks.png" className="grayscale mt-4 opacity-70 m-auto xl:m-0" width={412} height={39} alt="online class" />
+                </div>
+                <Image src="/images/programmer-landing.svg" width={702} height={521} alt="online class" />
+            </div>
+        </section>
+        `);
+  });
+});
