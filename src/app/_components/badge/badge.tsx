ac2880a0b367680ab3781b2e0583d1e7bd1b3a20
Damoon Darvishi <damoon.darvishi@gmail.com>
2023-11-13 23:14:43 +0330

    cerate & add badge component and css

---
+++ src/app/_components/badge/badge.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { BadgeProps } from "./badge.types";
+import { Size } from "../types/size.type";
+
+const sizeClasses: Record<Size, string> = {
+    tiny: 'badge-xs',
+    small: 'badge-sm',
+    normal: 'badge-md',
+    large: 'badge-lg'
+}
+
+export const Badge: React.FC<BadgeProps> = ({
+    variant,
+    className,
+    size = "tiny",
+    children
+}) => {
+    const classes = clsx('badge', className, {
+        [`badge-${variant}`] : variant,
+        [`${sizeClasses[size]}`] : size
+    })
+    return (
+        <span className={classes}>
+            {children}
+        </span>
+    )
+}
