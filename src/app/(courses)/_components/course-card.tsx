1a78c6118d4100714f138845509fe3c501584537
Damoon Darvishi <damoon.darvishi@gmail.com>
2023-12-05 22:54:08 +0330

    add price compponent

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,4 +1,6 @@
 import { Badge } from "@/app/_components/badge";
+import { IconArrowLeftFill, IconClock } from "@/app/_components/icons/icons";
+import { Price } from "@/app/_components/price/price";
 import { CourseSummary } from "@/types/course-summary.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,39 +8,47 @@ import Link from "next/link";
 export type CourseCardProps = CourseSummary & {};
 
 export const CourseCard: React.FC<CourseCardProps> = ({
-    coverImageId,
-    level,
-    title,
-    subTitle,
-    recordStatus,
-    basePrice,
-    duration,
-    slug
+  coverImageId,
+  level,
+  title,
+  subTitle,
+  recordStatus,
+  basePrice,
+  duration,
+  slug,
 }: CourseCardProps) => {
-    return (
-        <div className="card">
-            <figure>
-                <Image src={`https://api.classbon.com/api/picture/${coverImageId}`} 
-                alt={title}
-                width={550}
-                height={327}
-                />
-            </figure>
-            <div className="mt-2 flex gap-2 font-semibold dark:text-info px-3 py-2">
-                <Badge variant="info">{recordStatus}</Badge>
-                <Badge variant="accent">{level}</Badge>
-            </div>
-            <div className="card-body">
-                <Link href={`/course/${slug}`}>{title}</Link>
-                <p>{subTitle}</p>
-                <div>
-                <Badge variant="warning">{duration}</Badge>
-                    {basePrice}
-                </div>
-            </div>
-            <Link href={`/course/${slug}`} className="card-footer justify-center">
-                مشاهده جزيیات دوره
-            </Link>
+  return (
+    <div className="card">
+      <figure>
+        <Image
+          src={`https://api.classbon.com/api/picture/${coverImageId}`}
+          alt={title}
+          width={550}
+          height={327}
+        />
+      </figure>
+      <div className="mt-2 flex gap-2 font-semibold dark:text-info px-3 py-2">
+        <Badge variant="info">{recordStatus}</Badge>
+        <Badge variant="accent">{level}</Badge>
+      </div>
+      <div className="card-body">
+        <Link href={`/course/${slug}`}>{title}</Link>
+        <p>{subTitle}</p>
+        <div className="flex items-center justify-between mt-3">
+          <Badge variant="warning">
+            <IconClock width={16} height={16} stroke="red" />
+            {duration}
+          </Badge>
+          <Price price={basePrice} size="small" />
         </div>
-    )
-}
+      </div>
+      <Link
+        href={`/course/${slug}`}
+        className="card-footer justify-center animated-icon"
+      >
+        مشاهده جزيیات دوره
+        <IconArrowLeftFill fill="currentColor" />
+      </Link>
+    </div>
+  );
+};
