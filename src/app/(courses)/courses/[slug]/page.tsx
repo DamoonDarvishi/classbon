206ad5134d3af757726a87d0d08b5054d9c5b162
Damoon Darvishi <damoon.darvishi@gmail.com>
2023-12-25 23:21:22 +0330

    accordion feature component

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,41 +1,53 @@
 import { Progress } from "@/app/_components/progress";
 import { Rating } from "@/app/_components/rating";
 import { API_URL } from "@/configs/global";
+import { Accordion as AccordionType } from "@/types/accordion";
 import { CourseDetails } from "@/types/course-details.interface";
 
 async function getCourse(slug: string): Promise<CourseDetails> {
-    const res = await fetch(`${API_URL}/courses/${slug}`);
-    return res.json();
+  const res = await fetch(`${API_URL}/courses/${slug}`);
+  return res.json();
 }
 
-export default async function CourseDetails({params}: {params: {slug: string}}) {
-    const { slug } = params;
-    const course = await getCourse(slug);
-    return (
-        <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10 ">
-            <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
-            <div className="col-span-10 xl:col-span-7">
-                <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
-                    {course.title}
-                </h1>
-                <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
-                    {course.subTitle}
-                </h2>
+export default async function CourseDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+  const course = await getCourse(slug);
 
-                <div className="mt-5">Video Player Component</div>
-            </div>
-            <div className="col-span-10 xl:col-span-3">
-                <Rating rate={3} size="small" variant="info"/>
-                <Progress value={75}/>
-                <Progress value={75} variant="warning" size="tiny"/>
-                <Progress value={75} variant="error" size="tiny"/>
-                <Progress value={75} variant="info" size="tiny"/>
-            </div>
-            <div className="col-span-10 xl:col-span-6 bg-info">2</div>
-            <div className="col-span-10 xl:col-span-4 bg-warning">
-                <div>3</div>
-                <div>3</div>
-            </div>
-        </div>
-    )
+  const faqs: AccordionType[] = course.frequentlyAskedQuestions.map((faq) => ({
+    id: faq.id,
+    title: faq.question,
+    content: faq.answer,
+  }));
+
+  return (
+    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10 ">
+      <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
+      <div className="col-span-10 xl:col-span-7">
+        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+          {course.title}
+        </h1>
+        <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+          {course.subTitle}
+        </h2>
+
+        <div className="mt-5">Video Player Component</div>
+      </div>
+      <div className="col-span-10 xl:col-span-3">
+        <Rating rate={3} size="small" variant="info" />
+        <Progress value={75} />
+        <Progress value={75} variant="warning" size="tiny" />
+        <Progress value={75} variant="error" size="tiny" />
+        <Progress value={75} variant="info" size="tiny" />
+      </div>
+      <div className="col-span-10 xl:col-span-6 bg-info">2</div>
+      <div className="col-span-10 xl:col-span-4 bg-warning">
+        <div>3</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
 }
