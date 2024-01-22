ca43d9eb2f7733cd870f6369dc489dfba0f425a6
damoon darvishi <damoon.darvishi@gmail.com>
2024-01-22 17:21:12 +0330

    jest-test results commented on gitignore

--- src/app/(courses)/_components/course-card-list.tsx
+++ src/app/(courses)/_components/course-card-list.tsx
@@ -4,35 +4,29 @@ import { CourseCard } from "./course-card";
 import { API_URL } from "@/configs/global";
 
 type CourseCardListProps = {
-    courses: CourseSummary[];
-}
+  courses: CourseSummary[];
+};
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const res = await fetch(
-      `${API_URL}/courses/newest/${count}`,
-      {
-        cache: "no-store",
-        next: {
-          revalidate: 24 * 60 * 60,
-        },
-      }
-    );
-    return res.json();
-  }
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const res = await fetch(`${API_URL}/courses/newest/${count}`, {
+    cache: "no-store",
+    next: {
+      revalidate: 24 * 60 * 60,
+    },
+  });
+  return res.json();
+}
 
 export const CourseCardList: React.FC<CourseCardListProps> = async ({
-    courses
+  courses,
 }: CourseCardListProps) => {
   const newestCoursesData = await getNewestCourses(4);
-  console.log(newestCoursesData)
-    return (
-        <div className="flex flex-wrap justify-center xl:justify-start gap-6 mt-10">
-            {newestCoursesData.map((course) => {
-                return (
-                    <CourseCard key={`course-${course.slug}`} {...course} />
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-wrap justify-center xl:justify-start gap-6 mt-10">
+      {newestCoursesData.map((course) => {
+        return <CourseCard key={`course-${course.slug}`} {...course} />;
+      })}
+    </div>
+  );
+};
