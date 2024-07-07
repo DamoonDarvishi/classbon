0fcdbb5730381ee58aec287e08ff914264362048
damoon darvishi <damoon.darvishi@gmail.com>
2024-07-08 00:18:51 +0200

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
 import "./globals.css";
-import { Figtree } from "next/font/google";
+// import { Figtree } from "next/font/google";
 import localFont from "next/font/local";
 import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 
-const figtree = Figtree({
-  display: "swap",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-figtree",
+// const figtree = Figtree({
+//   display: "swap",
+//   subsets: ["latin"],
+//   weight: ["300", "400", "500", "600", "700", "800", "900"],
+//   variable: "--font-figtree",
+// });
+const source = localFont({
+  src: [
+    {
+      path: "../../public/fonts/static/SourceCodePro-Regular.ttf",
+      weight: "300",
+      style: "normal",
+    },
+  ],
+  variable: "--font-source",
 });
 
 const yekan = localFont({
@@ -28,11 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={`dark ${figtree.variable} ${yekan.variable}`}
-      dir="ltr"
-    >
+    <html lang="en" className={`dark ${source.variable}`} dir="ltr">
       <body className="min-h-screen grid grid-rows-[80px_1fr_auto] font-bold uppercase dark:bg-base-100 dark:text-base-content">
         <Header />
         <main>{children}</main>
